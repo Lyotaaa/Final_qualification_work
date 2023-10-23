ac4196550f140a71febd1debdcc56af478d492fc
Lyota <korystin.aleksei@yandex.ru>
2023-10-23 21:50:59 +0300

    update models

--- orders/app/models.py
+++ orders/app/models.py
@@ -162,7 +162,7 @@ class ProductInfo(models.Model):
     shop = models.ForeignKey(
         Shop,
         verbose_name="Магазин",
-        related_name="product_info",a
+        related_name="product_info",
         blank=True,
         on_delete=models.CASCADE,
     )
