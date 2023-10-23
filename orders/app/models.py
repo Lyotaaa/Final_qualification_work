a51e78d8c385af3eccf857eb61a89784c9e97760
Lyota <korystin.aleksei@yandex.ru>
2023-10-23 22:32:42 +0300

    update models

--- orders/app/models.py
+++ orders/app/models.py
@@ -311,8 +311,8 @@ class ConfirmEmailToken(models.Model):
         on_delete=models.CASCADE,
     )
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name=_("Когда был сгенерирован этот токен."
-    ))
+        auto_now_add=True, verbose_name=_("Когда был сгенерирован этот токен.")
+    )
     # Ключевое поле, хотя оно и не является первичным ключом модели
     key = models.CharField(
         _("Key"),
