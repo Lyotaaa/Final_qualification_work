d724409ff67d5101c6af357790d602e944113f34
Lyota <korystin.aleksei@yandex.ru>
2023-10-23 21:46:36 +0300

    update models

--- orders/app/models.py
+++ orders/app/models.py
@@ -53,7 +53,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
     REQUIRED_FIELDS = []
-    objects = UserManager
+    objects = UserManager()
     USERNAME_FIELD = "email"
     email = models.EmailField(_("email address"), unique=True)
     company = models.CharField(verbose_name="Компания", max_length=40, blank=True)
