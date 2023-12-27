73919167aa9ea78840ae7e2bfc9b01cc7a5ce7cd
Lyota <korystin.aleksei@yandex.ru>
2023-12-27 15:48:54 +0300

    update models

--- My_project/backend/models.py
+++ My_project/backend/models.py
@@ -44,11 +44,16 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
         if extra_fields.get("is_staff") is not True:
-            raise ValueError("У суперпользователя должно быть is_staff =True")
+            raise ValueError("У суперпользователя должно быть is_staff=True")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(
                 "У суперпользователя должно быть значение is_superuser=True."
+            )
+        if extra_fields.get("is_active") is not True:
+            raise  ValueError(
+                "Супер пользователь должны быть is_active=True"
             )
         return self._create_user(email, password, **extra_fields)
 
