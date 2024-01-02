0c5a5fc9a2c17f1d6b4d54bf4ac81f908fb72c3b
Lyota <korystin.aleksei@yandex.ru>
2024-01-02 17:22:54 +0300

    Проба

--- My_project/backend/serializers.py
+++ My_project/backend/serializers.py
@@ -13,40 +13,133 @@ from backend.models import (
 
 
 class ContactSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Contact
+        fields = (
+            "id",
+            "city",
+            "street",
+            "house",
+            "structure",
+            "building",
+            "apartment",
+            "user",
+            "phone",
+        )
+        read_only_fields = ("id",)
+        extra_kwargs = {"user": {"write_only": True}}
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    contacts = ContactSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "company",
+            "position",
+            "contacts",
+        )
+        read_only_fields = ("id",)
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Category
+        fields = (
+            "id",
+            "name",
+        )
+        read_only_fields = ("id",)
 
 
 class ShopSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Shop
+        fields = (
+            "id",
+            "name",
+            "state",
+        )
+        read_only_fields = ("id",)
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    pass
+    category = serializers.StringRelatedField()
+
+    class Meta:
+        model = Product
+        fields = (
+            "name",
+            "category",
+        )
 
 
 class ProductParameterSerializer(serializers.ModelSerializer):
-    pass
+    parameter = serializers.StringRelatedField()
+
+    class Meta:
+        model = ProductParameter
+        fields = (
+            "parameter",
+            "value",
+        )
 
 
 class ProductInfoSerializer(serializers.ModelSerializer):
-    pass
+    product = ProductSerializer(read_only=True)
+    product_parameters = ProductParameterSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = ProductInfo
+        fields = (
+            "id",
+            "model",
+            "product",
+            "shop",
+            "quantity",
+            "price",
+            "price_rrc",
+            "product_parameters",
+        )
+        read_only_fields = ("id",)
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = OrderItem
+        fields = (
+            "id",
+            "product_info",
+            "quantity",
+            "order",
+        )
+        read_only_fields = ("id",)
+        extra_kwargs = {"order": {"write_only": True}}
 
 
-class OrderItemCreateSerializer(serializers.Model):
-    pass
+class OrderItemCreateSerializer(OrderItemSerializer):
+    product_info = ProductInfoSerializer(read_only=True)
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    pass
+    ordered_items = OrderItemCreateSerializer(read_only=True, many=True)
+
+    total_sum = serializers.IntegerField()
+    contact = ContactSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "ordered_items",
+            "state",
+            "dt",
+            "total_sum",
+            "contact",
+        )
+        read_only_fields = ("id",)
