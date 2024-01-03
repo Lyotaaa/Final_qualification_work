2ed253fedb3e576e1d4cf8af948f1fc76e8168ef
Lyota <korystin.aleksei@yandex.ru>
2024-01-03 22:28:29 +0300

    update admin.py

--- My_project/backend/admin.py
+++ My_project/backend/admin.py
@@ -18,12 +18,8 @@ from backend.models import (
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    """
-    Панель управления пользователями
-    """
-
+    # Панель управления пользователями
     model = User
-
     fieldsets = (
         (None, {"fields": ("email", "password", "type")}),
         (
@@ -45,55 +41,154 @@ class CustomUserAdmin(UserAdmin):
         ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+    )
 
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
-    pass
+    # Список магазинов
+    model = Shop
+    list_display = ["id", "name", "user", "state"]
+    search_fields = (
+        "name",
+        "user__first_name",
+        "user__last_name",
+    )
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    # Список категорий
+    model = Category
+    list_display = ["id", "name"]
+    search_fields = ("name",)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    # Список продуктов
+    model = Product
+    list_display = ["id", "name", "category"]
+    search_fields = (
+        "id",
+        "name",
+        "category__name",
+    )
 
 
 @admin.register(ProductInfo)
 class ProductInfoAdmin(admin.ModelAdmin):
-    pass
+    # Список информации о продукте
+    # model = ProductInfo
+    # fieldsets = (
+    #     (None, {'fields': ('product', 'model', 'external_id', 'quantity')}),
+    #     ('Цены', {'fields': ('price', 'price_rrc')}),
+    # )
+    # list_display = ('product', 'external_id', 'price', 'price_rrc', 'quantity')
+    # inlines = [ProductParameterInline]
+    model = ProductInfo
+    list_display = [
+        "id",
+        "model",
+        "external_id",
+        "product",
+        "shop",
+        "quantity",
+        "price",
+        "price_rrc",
+    ]
+    search_fields = (
+        "id",
+        "model",
+        "price",
+        "price_rrc",
+        "product__name",
+        "shop__name",
+        "quantity",
+    )
 
 
 @admin.register(Parameter)
 class ParameterAdmin(admin.ModelAdmin):
-    pass
+    # Список имен параметров
+    model = Parameter
+    list_display = ["id", "name"]
+    search_fields = ("name",)
 
 
 @admin.register(ProductParameter)
 class ProductParameterAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    pass
+    # Список параметров
+    model = ProductParameter
+    list_display = ["product_name", "parameter", "value"]
+    search_fields = ("parameter__name", "product_name", "value")
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    # Список контактов пользователя
+    model = Contact
+    list_display = (
+        "id",
+        "user",
+        "city",
+        "street",
+        "house",
+        "structure",
+        "building",
+        "apartment",
+        "phone",
+    )
+    search_fields = (
+        "id",
+        "user__first_name",
+        "user__last_name",
+        "city",
+        "street",
+        "phone",
+    )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    # Список заказов
+    model = Order
+    list_display = ["id", "user", "datatime", "contact"]
+    search_fields = (
+        "user__first_name",
+        "user__last_name",
+        "state",
+    )
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    # Список заказанных позиций
+    model = OrderItem
+
+    list_display = [
+        "id",
+        "order",
+        "category",
+        "shop",
+        "quantity",
+        "state",
+        "product_info",
+    ]
+    search_fields = (
+        "id",
+        "category",
+        "quantity",
+    )
 
 
 @admin.register(ConfirmEmailToken)
 class ConfirmEmailTokenAdmin(admin.ModelAdmin):
+    # Токены подтверждения Email
     list_display = (
         "user",
         "key",
--- My_project/backend/apps.py
+++ My_project/backend/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class BackendConfig(AppConfig):
+    #verbose_name = "Основная часть"
     default_auto_field = "django.db.models.BigAutoField"
     name = "backend"
