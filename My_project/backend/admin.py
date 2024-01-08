e348a1b1d49ad0d91cb0d2af61529e04b88f2553
Lyota <korystin.aleksei@yandex.ru>
2024-01-09 00:10:45 +0300

    add "id" int file admin

--- My_project/backend/admin.py
+++ My_project/backend/admin.py
@@ -40,7 +40,7 @@ class CustomUserAdmin(UserAdmin):
         ),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_display = ("id", "email", "first_name", "last_name", "is_staff")
     search_fields = (
         "first_name",
         "last_name",
