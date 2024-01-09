12753c5d5db72463437525f4e7355c456c737507
Lyota <korystin.aleksei@yandex.ru>
2024-01-09 20:23:29 +0300

    add fixture, tests: details, contact

--- My_project/tests/backend/test_backend.py
+++ My_project/tests/backend/test_backend.py
@@ -7,6 +7,10 @@ from rest_framework.authtoken.models import Token
 new_user = {
     "email": "popova_Olga@gmail.com",
     "password": "11ff22FF33cc44CC",
+    "first_name": "Виктория",
+    "last_name": "Плюшкина",
+    "company": "Microsoft Corporation",
+    "position": "manager",
 }
 
 dict_url = {
@@ -36,45 +40,80 @@ def user():
     user = User.objects.create_user(
         email=new_user["email"],
         password=new_user["password"],
+        first_name=new_user["first_name"],
+        last_name=new_user["last_name"],
+        company=new_user["company"],
+        position=new_user["position"],
     )
     return user
+
+
+@pytest.fixture
+def register_user(client):
+    data = {
+        "email": new_user["email"],
+        "password": new_user["password"],
+    }
+    response = client.post(dict_url["register"], data=new_user)
+    return response
+
+
+@pytest.fixture
+def confirm_user(client, user):
+    token = ConfirmEmailToken.objects.create(user_id=user.id).key
+    data = {
+        "email": new_user["email"],
+        "token": token,
+    }
+    response_confirm = client.post(dict_url["confirm"], data=data)
+    return response_confirm, token
+
+
+@pytest.fixture
+def login_user(client, user, confirm_user):
+    data = {
+        "email": new_user["email"],
+        "password": new_user["password"],
+    }
+    response_login = client.post(dict_url["login"], data=data)
+    return response_login
 
 
 @pytest.mark.django_db
 def test_user_register(client):
     user_count = User.objects.count()
-    assert user_count == 0
     user = User.objects.create_user(
         email=new_user["email"],
         password=new_user["password"],
     )
     data = {
-        "first_name": "Ольга",
-        "last_name": "Попова",
         "email": new_user["email"],
         "password": new_user["password"],
-        "username": "hatz",
-        "company": "Microsoft",
-        "position": "manager",
     }
     response = client.post(dict_url["register"], data=new_user)
+    # response = register_user
     user_email = User.objects.get(email=user.email).email
     assert response.status_code == 200
     assert user_email == new_user["email"]
+    assert user_count == 0
     assert User.objects.count() == user_count + 1
 
 
 @pytest.mark.django_db
-def test_user_register_confirm_and_test_user_login(client, user):
-    token = ConfirmEmailToken.objects.create(user_id=user.id).key
-    data = {
-        "email": new_user["email"],
-        "password": new_user["password"],
-        "token": token,
-        "is_active": True,
-    }
-    response_confirm = client.post(dict_url["confirm"], data=data)
-    response_login = client.post(dict_url["login"], data=data)
+def test_user_confirm_and_test_user_login(
+    client, user, register_user, confirm_user, login_user
+):
+    # token = ConfirmEmailToken.objects.create(user_id=user.id).key
+    # data = {
+    #     "email": new_user["email"],
+    #     "password": new_user["password"],
+    #     "token": token,
+    #     "is_active": True,
+    # }
+    # response_confirm = client.post(dict_url["confirm"], data=data)
+    # response_login = client.post(dict_url["login"], data=data)
+    response_confirm, token = confirm_user[0], confirm_user[1]
+    response_login = login_user
     user_email = User.objects.get(email=user.email).email
     user_token = Token.objects.get(user=user).key
     data_confirm = response_confirm.json()
@@ -88,6 +127,47 @@ def test_user_register_confirm_and_test_user_login(client, user):
 
 
 @pytest.mark.django_db
-def test_user_details_post_and_get(client, user):
-    data = {}
-    response = client.post(dict_url["details"], data=data)
+def test_user_details_post_and_get(
+    client, user, register_user, confirm_user, login_user
+):
+    data = {
+        "id": User.objects.get().id,
+        "email": "vatrushkina_Olga@gmail.com",
+        "first_name": "Ольга",
+        "last_name": "Ватрушкина",
+        "company": "Google",
+        "position": "clerck",
+        "contacts": [],
+    }
+    client.force_authenticate(user)
+    response_post = client.post(dict_url["details"], data=data)
+    response_get = client.get(dict_url["details"])
+    data_post = response_post.json()
+    data_get = response_get.json()
+    assert response_post.status_code == 200
+    assert data_post["Status"] == True
+    assert data_get == data
+
+
+@pytest.mark.django_db
+def test_user_contact_post_and_get(
+    client, user, register_user, confirm_user, login_user
+):
+    data = {
+        "id": 1,
+        "city": "Том",
+        "street": "Ленина",
+        "house": "1",
+        "structure": "2",
+        "building": "3",
+        "apartment": "4",
+        "phone": "+7-999-111-22-33",
+    }
+    client.force_authenticate(user)
+    response_post = client.post(dict_url["contact"], data=data)
+    response_get = client.get(dict_url["contact"])
+    data_post = response_post.json()
+    data_get = response_get.json()
+    assert response_post.status_code == 200
+    assert data_post["Status"] == True
+    assert data_get[0] == data
